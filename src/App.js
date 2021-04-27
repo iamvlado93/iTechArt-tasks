38ab2feb97860900b49a93ba09f909283e1e3b0a
Vladislav <vladrjeousski@gmail.com>
2021-04-27 10:41:48 +0300

    feat: fixed code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import './App.css';
 import Counter from './components/counter-constructor/counter-constructor';
+import React, {useState} from 'react';
 
 function App() {
+  const [counters, setCounters] = useState([])
+  const [sum, setSum] = useState(0)
+  
+  const createCounter = () => {
+    setCounters([...counters, {name:`${counters.length + 1}`}])
+    console.log(counters)
+  }
+
+  const getCounterValue = (value) => {
+    setSum(sum + value)
+  }
+
   return (
-    <div className='wrap'>
-      <Counter/>
+  <div className='wrap'>
+      <button onClick={createCounter}>Create</button>
+      {counters.map((counter) => (
+        <Counter getCounterValue={getCounterValue} key={counter.name}/> 
+      ))}
+      <span>Кол-во пол.кликов: {sum}</span>
     </div>
   );
 }
--- src/components/counter-constructor/counter-constructor.js
+++ src/components/counter-constructor/counter-constructor.js
@@ -1,80 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {} from './counter-constructor.css';
 
-class CounterComponent extends React.Component {
-    state = {
-        count: 0,
-        counters: [{index:'', value:''}]
+const Counter = ({getCounterValue}) => {
+    const [count, setCount] = useState(0);
+
+    const countPlus = () => {
+        setCount(count + 1)
+        getCounterValue(1)
+    }
+    
+    const countMinus = () => {
+        setCount(count - 1)
+        getCounterValue(1)
     }
 
-    addCounter = () => {
-        let newCounter = this.counters;
-        this.setState({ counters: this.state.counters.concat(newCounter)})
-    }
-
-    plus = () => {
-        this.setState({ count: this.state.count + 1 });
-    }
-
-    minus = () => {
-        this.setState({ count: this.state.count - 1 });
-    }
-
-    render() {
-        return (
-            <div className='main'>
-                {this.state.counters.map((index, value) => {
-                    return (
-                        <li key={index} className='counter'>
-                            <button onClick={this.plus} className='button button__plus'>+</button>
-                                {this.state.value}
-                            <button onClick={this.minus} className='button button__minus'>-</button>
-                        </li> 
-                    )
-                })}
-                <button onClick={this.addCounter} className='button button__add'>Add Counter</button>
-            </div>
-        )
-    }
+    return (
+        <div className='counter'>
+            <button onClick={countPlus} className='button'>+</button>
+                {count}
+            <button onClick={countMinus} className='button'>-</button>
+        </div>
+    );
 }
 
-export default CounterComponent; 
-
-// class CounterConstructorComponent extends React.Component {
-//     state = {
-//         count: 0,
-//         counters: [{index: 1, value: }]
-//     }
-
-//     addCounter = () => {
-//         const newCounter = this.state.counters;
-//         this.setState({ counters: this.state.counters.concat(newCounter) })
-//     }
-
-//     plus = () => {
-//         this.setState({ count: this.state.count + 1 });
-//     }
-        
-//     minus = () => {
-//         this.setState({ count: this.state.count - 1 });
-//     }
-    
-//     render() {
-//         return (
-//             <div className='main'>
-//                 {this.state.counters.map((index, value) => {
-//                     return (
-//                         <li key={index} className='counter'>
-//                             <button onClick={() => this.plus()} className='button button__plus'>+</button>
-//                                 {value}
-//                             <button onClick={() => this.minus()} className='button button__minus'>-</button>
-//                         </li> 
-//                     )
-//                 })}
-//                 <button onClick={this.addCounter} className='button button__add'>Add Counter</button>
-//             </div>
-//         )
-//     }
-// }
-
-// export default CounterConstructorComponent;
+export default Counter; 
