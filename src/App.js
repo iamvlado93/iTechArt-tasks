ad226d6ffd96632177943f207fff93379fabf31a
Vladislav <vladrjeousski@gmail.com>
2021-04-29 12:47:41 +0300

    feat: added router structure for styled-components

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import RegistrationForm from './modules/RegistrationForm/index';
 import Profile from './modules/RegistrationForm/Profile';
 import Counter from './modules/Counter';
 import CounterConstructor from './modules/CounterConstructor';
+import StyledComponents from './modules/StyledComponents';
 
 import {
   BrowserRouter as Router,
@@ -20,6 +21,7 @@ export default function App() {
         <Link to='/counter'><button>Counter</button></Link>
         <Link to='/counter-constructor'><button>Counter Constructor</button></Link>
         <Link to='/registration-form'><button>Registration Form</button></Link>
+        <Link to='/styled-components'><button>Styled Components</button></Link>
       </nav>
 
       <main className='main'>
@@ -27,14 +29,19 @@ export default function App() {
           <Route path="/counter">
             <Counter />
           </Route>
+
           <Route path="/counter-constructor">
             <CounterConstructor />
           </Route>
+
           <Route path="/registration-form">
             <RegistrationForm/>
           </Route>
-          <Route path="/profile">
-            <Profile/>
+            <Route path="/profile">
+              <Profile/>
+            </Route>
+          <Route path="/styled-components">
+            <StyledComponents />  
           </Route> 
         </Switch>
       </main>
---
+++ src/modules/StyledComponents/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './index.css';
+
+export default function StyleComponents () {
+
+    return (
+        <div>Привет</div>
+    );
+}
