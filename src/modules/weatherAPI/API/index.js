98776b9ba0eb9cf3dbb5fc0a42af31a5c58b8848
Vladislav <vladrjeousski@gmail.com>
2021-05-13 14:39:05 +0300

    feat: added material ui

--- src/modules/weatherAPI/API/index.js
+++ src/modules/weatherAPI/API/index.js
@@ -1,18 +1,16 @@
-/* eslint-disable import/prefer-default-export */
 import axios from 'axios';
 
 const URL = 'https://api.openweathermap.org/data/2.5/weather';
 const API_KEY = 'c01f33698b101f2f3261afeaa8e4976a';
 
-export const fetchWeather = async (query) => {
+const fetchWeather = async (query) => {
   const { data } = await axios.get(URL, {
     params: {
       q: query,
-      units: 'metric',
       APPID: API_KEY,
     },
   });
-  // eslint-disable-next-line no-console
-  console.log(data);
   return data;
 };
+
+export default fetchWeather;
--- src/modules/weatherAPI/index.js
+++ src/modules/weatherAPI/index.js
@@ -1,50 +1,145 @@
 import React, { useState } from 'react';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import { Button, Container, TextField, Box } from '@material-ui/core';
+import './styles.css';
 
-import { fetchWeather } from './API';
-import './index.css';
+import fetchWeather from './API';
+
+const useStyles = makeStyles({
+  boxStyle: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    background:
+      'linear-gradient(90deg, rgba(0,22,36,1) 0%, rgba(9,88,121,1) 48%, rgba(0,212,255,1) 100%)',
+    width: '90vw',
+  },
+
+  inputStyle: {
+    color: 'cyan',
+    label: {
+      color: 'cyan',
+    },
+  },
+
+  buttonStyle: {
+    margin: '20px',
+    background: 'none',
+    width: '100px',
+    height: '25px',
+    border: '1px solid cyan',
+  },
+
+  containerStyle: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-around',
+    background: 'transparent',
+    width: '35vw',
+    height: '50vh',
+    borderRadius: 20,
+    border: '2px solid cyan',
+  },
+
+  headerStyle: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '50px',
+    background: 'transparent',
+    borderRadius: 20,
+    border: '1px solid cyan',
+  },
+
+  headerTextStyle: {
+    color: 'cyan',
+    marginLeft: '10px',
+  },
+
+  tempStyle: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '40px',
+    background: 'transparent',
+    color: 'cyan',
+    fontWeight: '800',
+    borderRadius: 20,
+    border: '1px solid cyan',
+  },
+
+  infoStyle: {
+    height: '25vh',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    background: 'transparent',
+    color: 'cyan',
+    fontWeight: '600',
+    borderRadius: 20,
+    border: '1px solid cyan',
+  },
+});
 
 const App = () => {
   const [query, setQuery] = useState('');
   const [weather, setWeather] = useState({});
+  const [error, setError] = useState(false);
 
   const search = async () => {
-    const data = await fetchWeather(query);
-    setWeather(data);
-    setQuery('');
+    if (query === '') {
+      setError(true);
+    } else {
+      const data = await fetchWeather(query);
+      setWeather(data);
+      setQuery('');
+      setError(false);
+    }
   };
 
+  const classes = useStyles();
+
   return (
-    <div className="main-container">
-      <input
+    <Box className={classes.boxStyle}>
+      <TextField
+        required
+        label="City Name"
+        variant="filled"
         type="text"
-        className="search"
+        className={classes.inputStyle}
         placeholder="Search..."
         value={query}
+        error={error}
         onChange={(e) => setQuery(e.target.value)}
       />
-      <button className="button" onClick={search} type="button">
+      <Button className={classes.buttonStyle} onClick={search} type="button">
         Search
-      </button>
+      </Button>
       {weather.main && (
-        <div className="card">
-          <h2 className="city-name">
-            <span>{weather.name}</span>
-            <sup>{weather.sys.country}</sup>
+        <Container className={classes.containerStyle}>
+          <h2 className={classes.headerStyle}>
+            <span className={classes.headerTextStyle}>City: {weather.name}</span>
+            <span className={classes.headerTextStyle}>Country: {weather.sys.country}</span>
           </h2>
-          <div className="city-temp">{Math.round(weather.main.temp)}&deg;</div>
-          <div className="info">
+          <div className={classes.tempStyle}>Temp: {Math.round(weather.main.temp)}&deg;</div>
+          <div className={classes.infoStyle}>
             <img
               className="city-icon"
               src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
               alt={weather.weather[0].description}
             />
-            <p>Wind Speed: {weather.wind.speed}</p>
-            <p>Wind Degree: {weather.wind.deg}</p>
-            <p>Weather Description: {weather.weather[0].description}</p>
+            <Typography variant="subtitle">Wind Speed: {weather.wind.speed}</Typography>
+            <Typography variant="subtitle">Degree: {weather.wind.deg}</Typography>
+            <Typography variant="subtitle">
+              Description: {weather.weather[0].description}
+            </Typography>
           </div>
-        </div>
+        </Container>
       )}
-    </div>
+    </Box>
   );
 };
 
